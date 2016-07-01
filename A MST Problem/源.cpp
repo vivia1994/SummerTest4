cf74ed220782ccb064daced03098ce248f18b116
Vivia Lei <leiwei183@163.com>
2016-07-01 14:26:42 +0800

    A MST Problem

--- A MST Problem/源.cpp
+++ A MST Problem/源.cpp
@@ -2,74 +2,80 @@
 #include<cmath>
 #include<cstdio>
 #include<cstring>
+#include<string.h>
 using namespace std;
-const int infinity = 99999999;
-const int maxnum = 105;
-double map1[maxnum][maxnum];
-bool visited[maxnum];
-double low[maxnum];
-int nodenum;
+
+const int Max = 99999999;
+const int mx = 105;
+double result;
+double low[mx];
+double map[mx][mx];
+bool vis[mx];
+int nn;
 struct node
 {
 	int x;
 	int y;
 	int z;
 } nd[105];
-double prim()
+void Prim()
 {
-	int i, j, pos = 1;
-	double result, Min;
-	memset(visited, 0, sizeof(visited));//初始化都未标记
+	int  p = 1;
+	double  Min;
+	memset(vis, 0, sizeof(vis));//初始化都未标记
 	result = 0;
-	for (i = 1; i <= nodenum; i++)
-		low[i] = map1[pos][i];
-	visited[pos] = 1;//把1号作为起点
-	for (i = 2; i <= nodenum; i++) //这个i没有其他的意思就是一个循环次数
+	for (int i = 1; i <= nn; i++)
+		low[i] = map[p][i];
+	vis[p] = 1;//把1号作为起点
+	for (int i = 2; i <= nn; i++) //这个i没有其他的意思就是一个循环次数
 	{
-		Min = infinity;
-		pos = -1;//从1号开始找最小的边
-		for (j = 1; j <= nodenum; j++)
-			if (!visited[j] && Min>low[j])
+		Min = Max;
+		p = -1;//从1号开始找最小的边
+		for (int j = 1; j <= nn; j++)
+			if (!vis[j] && Min > low[j])
 			{
 				Min = low[j];
-				pos = j;
+				p = j;
 			}
-		if (pos == -1)
-			return -1;
-		visited[pos] = 1;//做到与1os号最近的边
-		result += Min;//加权值
-		for (j = 1; j <= nodenum; j++)
-			if (!visited[j] && low[j]>map1[pos][j])
-				low[j] = map1[pos][j];//这个就是替换未被标记的最小权值！
+		if (p == -1)
+			result = -1;
+		vis[p] = 1;//做到与1os号最近的边
+		result += Min;//加权值		for(int vv=0;vv<1;vv++){}
+
+		for (int j = 1; j <= nn; j++)
+			if (!vis[j] && low[j] > map[p][j])
+				low[j] = map[p][j];//这个就是替换未被标记的最小权值！		for(int vv=0;vv<1;vv++){}
 	}
-	return result;
 }
 int main()
 {
-	int n, i, j, t;
-	double lenth, ans;
-	cin >> t;
+	int n, t;		
+	double l, r;		
+	cin >> t;		
 	while (t--)
 	{
-		cin >> n;
-		nodenum = n;
-		for (i = 1; i <= nodenum; i++)
-			for (j = 1; j <= nodenum; j++)
-				map1[i][j] = infinity;
-		for (i = 1; i <= n; i++)
-			cin >> nd[i].x >> nd[i].y >> nd[i].z;
-		for (i = 1; i <= n; i++)
+		cin >> n;		
+		nn = n;		
+		for (int i = 1; i <= nn; i++)
+			for (int j = 1; j <= nn; j++)
+				map[i][j] = Max;		
+		for (int i = 1; i <= n; i++)
+			cin >> nd[i].x >> nd[i].y >> nd[i].z;		
+		for (int i = 1; i <= n; i++)
 		{
-			for (j = i + 1; j <= n; j++)
+			for (int j = i + 1; j <= n; j++)
 			{
-				lenth = sqrt((nd[i].x - nd[j].x)*(nd[i].x - nd[j].x) + (nd[i].y - nd[j].y)*(nd[i].y - nd[j].y) + (nd[i].z - nd[j].z)*(nd[i].z - nd[j].z));
-				map1[i][j] = map1[j][i] = lenth;
+				l = sqrt((nd[i].x - nd[j].x)*(nd[i].x - nd[j].x) + (nd[i].y - nd[j].y)*(nd[i].y - nd[j].y) + (nd[i].z - nd[j].z)*(nd[i].z - nd[j].z));
+				map[i][j] = map[j][i] = l;		
 			}
-		}
-		ans = prim();
-		if (ans == -1)
+		}		
+
+		Prim();		
+		for(int vv=0;vv<1;vv++){}
+		if (result == -1)
 			cout << "?" << endl;
 		else
-			printf("%.2lf\n", ans);
+			printf("%.2lf\n", result);		
 	}
+	return 0;
 }
